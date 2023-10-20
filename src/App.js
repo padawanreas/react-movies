c2a2f7bdb4848be6941ec84a00b1dbee7c6b4321
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-21 01:36:56 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./components/Home/Home";
 import NavBar from "./components/Nav/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
