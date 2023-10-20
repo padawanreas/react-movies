7e8589ef0555d179af4a31e873ac7e41bfd76121
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-21 01:36:30 +0200

    index

---
+++ src/components/Configs/RouteConfig.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RouteConfig() {
+  return <div>RouteConfig</div>;
+}
+
+export default RouteConfig;
--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-//import App from "./App";
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import NavBar from "./components/Nav/NavBar";
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
-*/
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <NavBar />,
-  },
-]);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
-/*reportWebVitals();*/
+
+reportWebVitals();
