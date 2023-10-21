e1212f81d875fccb9758630a4e587d8db6bcab64
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-21 18:44:04 +0200

    movies

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,7 +1,14 @@
 import React from "react";
+import MoviesBanner from "./MoviesBanner";
+import MoviesList from "./MoviesList";
 
 const Movies = () => {
-  return <div>Movies</div>;
+  return (
+    <>
+      <MoviesBanner />
+      <MoviesList />
+    </>
+  );
 };
 
 export default Movies;
