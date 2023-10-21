c07ad8eb7b97ef0adf0c813ce2d3176bb62d1192
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-21 17:07:11 +0200

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from "react";
 import HomeBanner from "./HomeBanner";
+import HomeList from "./HomeList";
 
 const Home = () => {
   return (
     <>
       <HomeBanner />
-      <div>home list</div>
+      <HomeList />
     </>
   );
 };
