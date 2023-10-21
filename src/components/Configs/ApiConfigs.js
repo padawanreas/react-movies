994c0c6fbf44a20b6236ec518feeb47bc5e5d16e
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-21 18:09:58 +0200

    api config

--- src/components/Configs/ApiConfigs.js
+++ src/components/Configs/ApiConfigs.js
@@ -1,2 +1,9 @@
 export const API_KEY = process.env.REACT_APP_THE_MOVIE_API_KEY;
 export const BASE_URL = "https://api.themoviedb.org/3";
+
+const requests = {
+  trendingMovies: `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`,
+  trendingTv: `${BASE_URL}/trending/tv/week?api_key=${API_KEY}`,
+};
+
+export default requests;
