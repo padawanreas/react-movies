1d0f518e7e83d9bfddeb0788c256a7f17719d362
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-22 16:01:22 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import Home from "./components/Home/Home";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Movies from "./components/Movies/Movies";
-import Series from "./components/Series/Series";
+import React from "react";
 import NavBar from "./components/Nav/NavBar";
-import Detail from "./components/Detail/Detail";
-//import Root from "./components/Configs/Root";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <NavBar />
-      </div>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "movie",
-        element: <Movies />,
-      },
-      {
-        path: "tv",
-        element: <Series />,
-      },
-      {
-        path: "detail",
-        element: <Detail />,
-      },
-    ],
-  },
-]);
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <NavBar />
     </>
   );
 }
