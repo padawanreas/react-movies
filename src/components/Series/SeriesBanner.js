37b446de99c126e80f4c72652ea0b4cc3f58277e
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-22 13:50:32 +0200

    banner series

---
+++ src/components/Series/SeriesBanner.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { API_KEY, BASE_URL } from "../Configs/ApiConfigs";
+import axios from "axios";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import "../Home/Home.css";
+
+function SeriesBanner() {
+  const [series, setSeries] = useState([]);
+  useEffect(() => {
+    const fetchserie = async () => {
+      const response = await axios.get(
+        `${BASE_URL}/tv/on_the_air?api_key=${API_KEY}&language=fr-FR`
+      );
+      setSeries(response.data.results);
+    };
+    fetchserie();
+  }, []);
+
+  console.log(series);
+
+  return (
+    <>
+      <div className="banner_content">
+        <Swiper
+          centeredSlides={true}
+          loop={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <div lassName="banner__content">
+            {series.map((serie) => (
+              <SwiperSlide key={serie.id}>
+                <img
+                  key={serie.id}
+                  src={`https://image.tmdb.org/t/p/original/${serie.backdrop_path}`}
+                  alt={serie.title || serie.original_title || serie.name}
+                  className="banner__images"
+                />
+
+                {
+                  <div className="banner__text__content">
+                    <div className="banner__text">
+                      <h1>
+                        {serie.title || serie.original_title || serie.name}
+                      </h1>
+                      <p>{serie.overview}</p>
+
+                      <button className="btn-info">More info</button>
+                    </div>
+                  </div>
+                }
+              </SwiperSlide>
+            ))}
+          </div>
+        </Swiper>
+      </div>
+    </>
+  );
+}
+
+export default SeriesBanner;
