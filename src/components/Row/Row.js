5cef24f0400664b0294def14472b3f00b39b141b
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-22 14:38:28 +0200

    row

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -5,6 +5,7 @@ import "./Row.css";
 
 function Row({ title, fetchUrl }) {
   const [medias, setMedias] = useState([]);
+
   useEffect(() => {
     const fetchMedia = async () => {
       const response = await axios.get(fetchUrl);
@@ -17,19 +18,20 @@ function Row({ title, fetchUrl }) {
     <div className="row">
       <h2 className="row__title">{title}</h2>
       <div className="row__images">
-        {medias.map((media) => (
-          <div key={media.id}>
-            <Link to="/detail">
-              {
-                <img
-                  src={`https://image.tmdb.org/t/p/original/${media.backdrop_path}`}
-                  className="row__image"
-                  alt={media?.title || media?.name || media?.original_title}
-                />
-              }
-            </Link>
-          </div>
-        ))}
+        {medias &&
+          medias.map((media) => (
+            <div key={media.id}>
+              <Link to="/detail">
+                {
+                  <img
+                    src={`https://image.tmdb.org/t/p/original/${media.backdrop_path}`}
+                    className="row__image"
+                    alt={media?.title || media?.name || media?.original_title}
+                  />
+                }
+              </Link>
+            </div>
+          ))}
       </div>
     </div>
   );
