d2a04ff2f156524f8cb66d92cb74d45e8eae35c7
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-28 15:56:37 +0200

    api config

--- src/components/Configs/ApiConfigs.js
+++ src/components/Configs/ApiConfigs.js
@@ -2,14 +2,14 @@ export const API_KEY = process.env.REACT_APP_THE_MOVIE_API_KEY;
 export const BASE_URL = "https://api.themoviedb.org/3";
 
 const requests = {
-  trendingMovies: `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`,
-  trendingTv: `${BASE_URL}/trending/tv/week?api_key=${API_KEY}`,
-  moviesUpcoming: `${BASE_URL}/movie/upcoming?api_key=${API_KEY}`,
-  moviesTopRated: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
-  moviesPopular: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
-  seriesPopular: `${BASE_URL}/tv/popular?api_key=${API_KEY}`,
-  seriesAirling: `${BASE_URL}/tv/airing_today?api_key=${API_KEY}`,
-  seriesTopRated: `${BASE_URL}/tv/top_rated?api_key=${API_KEY}`,
+  trending: (type) => `${BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
+  nowPlaying: (type) => `${BASE_URL}/${type}/now_playing?api_key=${API_KEY}`,
+  onTheAir: (type) => `${BASE_URL}/${type}/on_the_air?api_key=${API_KEY}`,
+  popular: (type) => `${BASE_URL}/${type}/popular?api_key=${API_KEY}`,
+  topRated: (type) => `${BASE_URL}/${type}/top_rated?api_key=${API_KEY}`,
+  upcoming: (type) => `${BASE_URL}/${type}/upcoming?api_key=${API_KEY}`,
+  airling: (type) => `${BASE_URL}/${type}/airing_today?api_key=${API_KEY}`,
+  detail: (type, id) => `${BASE_URL}/${type}/${id}?api_key=${API_KEY}`,
 };
 
 export default requests;
