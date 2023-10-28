920ae5d87cdd5ddb897f21f8e92ecbd5b1f6ae41
Rebecca Bossé <rebeccaabiro@gmail.com>
2023-10-28 16:51:55 +0200

    route config

---
+++ src/components/Configs/RouteConfig.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Detail from "../Detail/Detail";
+
+const RouteConfig = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/detail/:id/:name" element={<Detail />} />
+    </Routes>
+  );
+};
+
+export default RouteConfig;
